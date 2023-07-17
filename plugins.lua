409b22a9157788303f34f9a59eb1d78ca2cd9a84
jinzaizhichi <jinzaizhichi9888@gmail.com>
2023-07-17 23:01:09 +0800

    fix

--- plugins.lua
+++ plugins.lua
@@ -31,7 +31,7 @@ local plugins = {
       "rcarriga/nvim-dap-ui",
     },
     config = function(_, opts)
-     local path = '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+     local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
      require("dap-python").setup(path)
      require("core.utils").load_mappings("dap_python")
     end,
