be4c4a20811f631ff60f2ba44bce71a5daf2e524
jinzaizhichi <jinzaizhichi9888@gmail.com>
2023-07-17 21:35:24 +0800

    add debug

---
+++ mappings.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.general = {
+  n = {
+    [":"] = {":", "enter command mode", opts = {nowait = true}},
+     }
+ }
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+       require('dap_python').test_method()
+      end
+    }
+  }
+}
+return M
