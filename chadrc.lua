fc4766a1cef9079d42b551a3b9edc4f11cf1c388
jinzaizhichi <jinzaizhichi9888@gmail.com>
2023-07-22 13:35:20 +0800

    add wakatime chatgpt

--- chadrc.lua
+++ chadrc.lua
@@ -2,5 +2,15 @@
  local M = {}
  M.ui = {theme = 'gruvbox'}
  M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
+ -- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+M.mappings = require "custom.mappings"
  return M
