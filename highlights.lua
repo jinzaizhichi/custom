789e119a8780f481ce9cb699cf1dd65878aaa81f
jinzaizhichi <jinzaizhichi9888@gmail.com>
2023-07-22 13:46:53 +0800

    add wakatime chatgpt

--- highlights.lua
+++ highlights.lua
@@ -2,18 +2,18 @@
 -- Each highlight group can take a table with variables fg, bg, bold, italic, etc
 -- base30 variable names can also be used as colors
 
-local M = {}
-
----@type Base46HLGroupsList
-M.override = {
-  Comment = {
-    italic = true,
-  },
-}
-
----@type HLTable
-M.add = {
-  NvimTreeOpenedFolderName = { fg = "green", bold = true },
-}
-
-return M
+-- local M = {}
+--
+-- ---@type Base46HLGroupsList
+-- M.override = {
+--   Comment = {
+--     italic = true,
+--   },
+-- }
+--
+-- ---@type HLTable
+-- M.add = {
+--   NvimTreeOpenedFolderName = { fg = "green", bold = true },
+-- }
+--
+-- return M
